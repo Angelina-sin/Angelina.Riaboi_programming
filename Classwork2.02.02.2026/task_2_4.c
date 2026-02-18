db548064d19a576fa76cacae85cf2b2b32d56da1
Angelina-sin <angelinaryaboy2007@gmail.com>
2026-02-18 19:25:00 +0200

    Add files via upload

---
+++ Classwork2.02.02.2026/task_2_4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// // a)
+// double task2_4_func1(double x){
+//     double y = x*x+1;
+//     return y*y;
+// }
+
+// int main(){
+//     double x,y;
+//     printf("Enter x=");
+//     scanf("%lf",&x);
+//     y=task2_4_func1(x);
+//     printf("Result y=%6f\n", y);
+// }
+
+// в)y=x^5+5x^4+10x^3+10x^2+5x+1
+double task2_4_func3(double x){
+    double y=(((x+5)*x+10)*x+10)*x+5;
+    y=y*x+1;
+    return y;
+}
+
+int main(){
+    double x,y;
+    printf("Enter x=");
+    scanf("%lf",&x);
+    y = task2_4_func3(x);
+    printf("Result y=%6f\n", y);
+}
